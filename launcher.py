1815043c752a066dcd9dffd15ca10342e221001e
pmartins22 <phenriquecostamartins22@gmail.com>
2025-12-11 12:44:37 +0100

    feat(main_window.py): added main window

--- launcher.py
+++ launcher.py
@@ -1,2 +1,7 @@
+from tkinter import *
+from src.gui.main_window import *
+
 if __name__ == "__main__":
-    pass
+    root = Tk()
+    app = MainWindow(root)
+    root.mainloop()
---
+++ src/gui/main_window.py
@@ -0,0 +1,7 @@
+class MainWindow(object):
+    def __init__(self, root):
+        self.root = root
+        self.root.title("PixelCircleCalculator")
+        self.root.geometry("800x700")
+
+
