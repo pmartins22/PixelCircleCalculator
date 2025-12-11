65cebd05f59e31ee01a6dfad86a36e22c77a5288
pmartins22 <phenriquecostamartins22@gmail.com>
2025-12-11 13:15:12 +0100

    feat(main_window.py):
    - added main frame
    - added column and row resizing configuration

--- src/gui/main_window.py
+++ src/gui/main_window.py
@@ -1,7 +1,21 @@
-class MainWindow(object):
+import tkinter as tk
+from tkinter import ttk
+
+from src.gui.grid_frame import GridFrame
+
+
+class MainWindow:
     def __init__(self, root):
         self.root = root
         self.root.title("PixelCircleCalculator")
         self.root.geometry("800x700")
+
+        root.columnconfigure(0, weight=1)
+        root.rowconfigure(0, weight=1)
+
+        main_frame = ttk.Frame(root, padding="10")
+        main_frame.grid(row=0, column=0, sticky=tk.NSEW)
+        main_frame.columnconfigure(0, weight=1)
+        main_frame.rowconfigure(0, weight=1)
 
 
