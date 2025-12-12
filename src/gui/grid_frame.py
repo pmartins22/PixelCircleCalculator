b773e5858a274d887bd3b494afd2f8ec40317505
pmartins22 <phenriquecostamartins22@gmail.com>
2025-12-12 16:09:22 +0100

    feat(grid_frame.py):
    - added grid and pixel initial sizes
    - added initial zoom
    - added color constants

--- src/gui/grid_frame.py
+++ src/gui/grid_frame.py
@@ -4,5 +4,13 @@ class GridFrame(ttk.Frame):
     def __init__(self, parent):
         super().__init__(parent)
 
+        self.base_pixel_size = 20
+        self.pixel_size = 20
+        self.grid_size = 10
+        self.zoom_level = 1.0
 
+        self.COLOR_GRID_LINE = "#CCCCCC"
+        self.COLOR_BACKGROUND = "#FFFFFF"
+        self.COLOR_AXIS = "#666666"
+        self.COLOR_PIXEL_FILLED = "#000000"
 
