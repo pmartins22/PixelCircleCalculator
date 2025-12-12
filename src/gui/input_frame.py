31cb6b2b822ef04462e91a5c129b5f835e0cca02
pmartins22 <phenriquecostamartins22@gmail.com>
2025-12-12 15:51:54 +0100

    feat(input_frame.py):
    - added radius label

--- src/gui/input_frame.py
+++ src/gui/input_frame.py
@@ -2,6 +2,12 @@ import tkinter as tk
 from tkinter import ttk
 
 class InputPanel(ttk.Frame):
-    def __init__(self, parent):
+    def __init__(self, parent, on_calculate_callback):
         super().__init__(parent)
+
+        self.on_calculate = on_calculate_callback
+
+        label = ttk.Label(self, text="Radius:")
+        label.grid(row=0, column=0, padx=5)
+
 
