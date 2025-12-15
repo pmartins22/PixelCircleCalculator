5fa6de234830779a3b3fdb88838a64f4aaed1a34
pmartins22 <phenriquecostamartins22@gmail.com>
2025-12-15 15:14:52 +0100

    feat(circle_calculator.py):
    - added circle application to table
    - added float return option to _find_center function

--- src/core/circle_calculator.py
+++ src/core/circle_calculator.py
@@ -12,6 +12,8 @@ class CircleCalculator:
 
     def _setup(self):
         self.table = [['0'] * 10 for _ in range(10)]
+        self.set_radius(1)
+        self.print_table()
 
     def set_radius(self, radius):
         if radius < 1:
@@ -22,6 +24,7 @@ class CircleCalculator:
         self.table = [['0'] * grids_number for _ in range(grids_number)]
 
         self._apply_skeleton_tiles(radius)
+        self._apply_circle_tiles(radius)
 
     def _apply_skeleton_tiles(self, radius):
         center = self._find_center()
@@ -38,10 +41,74 @@ class CircleCalculator:
             if self.table[center][i] != '1':
                 self.table[center][i] = '2'
 
-    def _find_center(self):
+    def _apply_circle_tiles(self, radius):
+        if radius == 1:
+            return
+
+        for y in range(len(self.table)):
+            for x in range(len(self.table[y])):
+                if self._pixel_intercepts_circle(radius, x, y) and self.table[y][x] != '1' and self.table[y][x] != '2':
+                    self.table[y][x] = '3'
+
+    def _pixel_intercepts_circle(self, radius, px, py):
+        center = self._find_center(as_float=True)
+        radius = radius - 1
+        # Calculate smaller x distance to center
+        px1 = px
+        px2 = px + 1
+
+        if px1 <= center <= px2:
+            smaller_point_x = center
+        else:
+            if abs(center - px1) < abs(center - px2):
+                smaller_point_x = px1
+            else:
+                smaller_point_x = px2
+
+
+        # Calculate smaller y distance to center
+
+        # Translate pixel y to cartesian y
+        py1 = center * 2 - py - 1
+        py2 = py1 + 1
+
+        if py1 <= center <= py2:
+            smaller_point_y = center
+        else:
+            if abs(center - py1) < abs(center - py2):
+                smaller_point_y = py1
+            else:
+                smaller_point_y = py2
+
+        smaller_dist_to_center = math.dist([center, center], [smaller_point_x, smaller_point_y])
+
+        # Calculate larger x distance to center
+        if px1 <= center <= px2:
+            larger_point_x = center + 0.5
+        else:
+            if abs(center - px1) > abs(center - px2):
+                larger_point_x = px1
+            else:
+                larger_point_x = px2
+
+        # Calculate larger y distance to center
+        if py1 <= center <= py2:
+            larger_point_y = center + 0.5
+        else:
+            if abs(center - py1) > abs(center - py2):
+                larger_point_y = py1
+            else:
+                larger_point_y = py2
+
+        larger_dist_to_center = math.dist([center, center], [larger_point_x, larger_point_y])
+
+        return smaller_dist_to_center < radius < larger_dist_to_center
+
+    def _find_center(self, as_float=False):
         size = len(self.table)
-        center = size // 2
-        return center
+        if as_float:
+            return size / 2
+        return size // 2
 
     def print_table(self):
         for col in range(len(self.table)):
